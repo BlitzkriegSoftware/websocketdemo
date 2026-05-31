f5b24096312a97ccbd071e5290a6b2080327e29f
Stuart Williams <spookdejur@hotmail.com>
2026-05-31 12:14:27 -0700

    fastapi,3

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
+
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
-
-app = FastAPI()
+import uvicorn
 
 
 # Manage multiple ws connections
@@ -20,16 +21,25 @@ class ConnectionManager:
         for connection in self.active_connections:
             await connection.send_text(message)
 
+
 manager = ConnectionManager()
+
+# Read in PORT env var
+port = int(os.environ.get("PORT", 8000))
+
+app = FastAPI()
 
 # Read in chat app page
 with open("index.html", "r", encoding="utf-8") as file:
     html = file.read()
+    html = html.replace("##PORT##", str(port))
+
 
 # Serve chat app page
 @app.get("/")
 async def get():
     return HTMLResponse(html)
+
 
 # Serve websocket chat
 @app.websocket("/ws")
@@ -42,3 +52,13 @@ async def websocket_endpoint(websocket: WebSocket):
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         print("Client disconnected")
+
+
+if __name__ == "__main__":
+    print("Port: ", port)
+    uvicorn.run(
+        "main:app",  # Replace 'main' with your actual file name
+        host="127.0.0.1",  # Use "0.0.0.0" to accept external requests
+        port=port,  # Set your custom port number here
+        reload=True,  # Enables auto-reload during development
+    )
